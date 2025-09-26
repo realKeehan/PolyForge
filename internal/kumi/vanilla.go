3657ba79eaf4528decb1b08d760abcdc57895ad3
Keehan <41766284+realKeehan@users.noreply.github.com>
2025-09-26 08:50:43 -0700

    refactor: reorganize kumi structure

--- internal/kumi/vanilla.go
+++ internal/kumi/vanilla.go
@@ -1,52 +1,7 @@
 package kumi
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-)
+import "polyforge/internal/kumi/install"
 
 func (s *Service) installVanilla() (*ActionResult, error) {
-	result := newResult()
-	home, err := os.UserHomeDir()
-	if err != nil {
-		result.Error(fmt.Sprintf("unable to resolve user home: %v", err))
-		result.Success = false
-		return result, nil
-	}
-
-	vanillaDir := filepath.Join(home, "AppData", "Roaming", "KUMIProfiles", "TurtelSMP5")
-	if err := ensureDir(vanillaDir); err != nil {
-		result.Error(fmt.Sprintf("failed to create vanilla directory: %v", err))
-		result.Success = false
-		return result, nil
-	}
-
-	if err := s.downloadAndExtract(vanillaZipURL, vanillaDir, ""); err != nil {
-		result.Error(fmt.Sprintf("vanilla download failed: %v", err))
-		result.Success = false
-		return result, nil
-	}
-	result.Info(fmt.Sprintf("Installed vanilla files to %s", vanillaDir))
-
-	versionsDir := filepath.Join(home, "AppData", "Roaming", ".minecraft", "versions")
-	if pathExists(versionsDir) {
-		if err := s.downloadAndExtract(quiltLoaderZipURL, versionsDir, ""); err != nil {
-			result.Warning(fmt.Sprintf("Quilt loader download failed: %v", err))
-		} else {
-			result.Info("Installed Quilt Loader profile")
-		}
-	} else {
-		result.Warning(fmt.Sprintf("Minecraft versions directory not found at %s", versionsDir))
-	}
-
-	launcherJSON := filepath.Join(home, "AppData", "Roaming", ".minecraft", "launcher_profiles.json")
-	if err := addLauncherProfile(launcherJSON); err != nil {
-		result.Warning(fmt.Sprintf("Failed to update launcher profiles: %v", err))
-	} else {
-		result.Info("Ensured launcher profile 'turtelsmp'")
-	}
-
-	result.Success = true
-	return result, nil
+	return install.Vanilla(s.installDependencies(), vanillaZipURL, quiltLoaderZipURL)
 }
